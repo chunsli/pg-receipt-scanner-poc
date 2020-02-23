c2531fe8b06a881ab5fc133598136d410211ee4e
Matt Li <matt@branch8.com>
2020-02-23 23:53:33 +0800

    lower case

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HomePage from '../src/containers/HomePage/index.js';
+import HomePage from '../src/containers/homepage';
 
 function ResponsiveDrawer() {
 

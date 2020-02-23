71745b4c2deb58a9df816f6d8c764fd6e71a5885
Matt Li <matt@branch8.com>
2020-02-23 23:45:13 +0800

    remove yarn.lock

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HomePage from '../src/containers/HomePage';
+import HomePage from '../src/containers/HomePage/index.js';
 
 function ResponsiveDrawer() {
 

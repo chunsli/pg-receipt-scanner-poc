0efa3a1bb5abf4ca0f5db03e8cf399e0d5873f32
tancc <vr2006ccc@gmail.com>
2020-07-10 16:15:39 +0800

    let pg product high priority

--- pages/shop/[catCode].js
+++ pages/shop/[catCode].js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Grid, Button, SvgIcon, Box} from '@material-ui/core'
+import { Grid, Button, SvgIcon, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import SortIcon from '@material-ui/icons/Sort';
 import { useRouter } from 'next/router'
 import SearchBar from '../../src/components/SearchBar';
 import { CatProductCard } from '../../src/components/CatProductCard';
 import Link from 'next/link'
-import FakeData from '../../src/utils/fake'
+import FakeData, { isPgBrand } from '../../src/utils/fake'
 
 
 function FilterIcon(props) {
   return (
     <SvgIcon {...props}>
-      <g><path d="M0,0h24 M24,24H0" fill="none"/><path d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"/><path d="M0,0h24v24H0V0z" fill="none"/></g>
+      <g><path d="M0,0h24 M24,24H0" fill="none" /><path d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z" /><path d="M0,0h24v24H0V0z" fill="none" /></g>
     </SvgIcon>
   );
 }
@@ -25,7 +25,7 @@ const cheapestPrice = (data) => Object.entries(data).reduce((cur, [key, value]) 
   }
 }, Number.POSITIVE_INFINITY)
 
-const useStyles = makeStyles(theme =>  ({
+const useStyles = makeStyles(theme => ({
   catImg: {
     objectFit: 'cover',
     width: '100%',
@@ -49,40 +49,55 @@ const useStyles = makeStyles(theme =>  ({
 
 const Category = () => {
   const classes = useStyles();
-  const {query: {catCode}} = useRouter()
+  const { query: { catCode } } = useRouter()
   const cat = FakeData.find(d => d.code === catCode)
+  const not0priceData = cat.data.filter(item => {
+    const cheapestP = cheapestPrice(item.data)
+    return cheapestP > 0
+  })
+  const pgData = not0priceData.filter(item => isPgBrand(item.brandEN))
+  const data = pgData.length > 20 ? pgData : not0priceData.sort((a, b) => {
+    const _a = isPgBrand(a.brandEN)
+    const _b = isPgBrand(b.brandEN)
+    if (_a === _b) {
+      return 0
+    } else if (_a) {
+      return -1
+    } else {
+      return 1
+    }
+  })
 
   return (
     <Grid container align-content-xs-space-around={'true'}>
       <Grid xs={12} item className={classes.search}>
         <SearchBar />
       </Grid>
-      <Grid container direction={'row'} align-content-xs-between={'true'}	className={classes.action}>
+      <Grid container direction={'row'} align-content-xs-between={'true'} className={classes.action}>
         <Grid xs={6} item >
-          <Button aria-label="sort" style={{color: '#64b5f6'}} fullWidth>
-            <SortIcon/> Sort
+          <Button aria-label="sort" style={{ color: '#64b5f6' }} fullWidth>
+            <SortIcon /> Sort
           </Button>
         </Grid>
-      
+
         <Grid xs={6} item >
-          <Button aria-label="filter" style={{color: '#64b5f6'}} fullWidth>
-            <FilterIcon/> Filter
+          <Button aria-label="filter" style={{ color: '#64b5f6' }} fullWidth>
+            <FilterIcon /> Filter
           </Button>
         </Grid>
       </Grid>
       <Box display="flex" flexDirection="column" px={2} width="100%">
-        {cat.data.filter(item => {
-          const cheapestP = cheapestPrice(item.data)
-          return cheapestP >0
-        }).map(item => {
-          return (
-            <Link key={item.code} href={`/shop/${catCode}/${item.code}`}>
-              <Box width="100%" mb={2}>
-                <CatProductCard {...item} />
-              </Box>
-            </Link>
-          )
-        })}
+        {data
+          .map(item => {
+
+            return (
+              <Link key={item.code} href={`/shop/${catCode}/${item.code}`}>
+                <Box width="100%" mb={2}>
+                  <CatProductCard {...item} />
+                </Box>
+              </Link>
+            )
+          })}
       </Box>
 
     </Grid>
